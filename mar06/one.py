1446110c253f4956bdea962b61d44109293f1e61
Joakim Wassberg <joakim@arthead.se>
2023-03-06 18:03:41 +0100

    Mar 06

---
+++ mar06/one.py
@@ -0,0 +1,15 @@
+name = 'Alice'
+age = 34
+height = 1.68
+active = True
+
+values = [1, 2, 3, 4, 5]
+more_values = (1, 2, 3, 4, 5)
+print(more_values)
+
+things = {
+    'name': 'Anna',
+    'age': 23
+}
+
+print(things['age'])
