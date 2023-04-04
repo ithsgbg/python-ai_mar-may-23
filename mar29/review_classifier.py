8fe26755837f3ca6f32401a53ee993705aab3d84
Joakim Wassberg <joakim@arthead.se>
2023-04-04 15:57:05 +0200

    Apr 03 2023

--- mar29/review_classifier.py
+++ mar29/review_classifier.py
@@ -2,6 +2,10 @@ import contractions
 import re
 import json
 from string import punctuation
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVC
+
 
 def clean_text(text):
     text = contractions.fix(text)
@@ -26,3 +30,33 @@ with open('./mar29/Pet_Supplies_1000.json', 'r') as in_file:
         review_text = data['reviewText']
         overall = data['overall']
         reviews.append(Review(review_text, overall))
+
+training_data, test_data = train_test_split(reviews, test_size=0.2, random_state=42)
+
+train_X = [review.cleaned for review in training_data]
+train_y = [review.sentiment for review in training_data]
+
+test_X = [review.cleaned for review in test_data]
+test_y = [review.sentiment for review in test_data]
+
+vectorizer = CountVectorizer()
+train_x_vectors = vectorizer.fit_transform(train_X)
+
+clf_svc = SVC(kernel='linear')
+clf_svc.fit(train_x_vectors, train_y)
+
+test_x_vectors = vectorizer.transform(test_X)
+# for i, vector in enumerate(test_x_vectors):
+#     prediction = clf_svc.predict(vector)
+#     if prediction[0] != test_y[i]:
+#         print('Predicted', prediction[0])
+#         print('Was', test_y[i])
+#         print("Review text:", test_X[i])
+
+#print(clf_svc.score(test_x_vectors, test_y))
+
+review_text = "I bought Teddy's Pride Oral Care with the hope it would help to keep my dog's teeth clean and freshen her breath like the product information says it will do. Unfortunately, it doesn't do anything! I've now used the entire jar of powder as directed, but the claims for cleaning a dog's teeth and freshening a dog's breath were not kept in the slightest. I certainly won't buy more! Total waste of money and disappointment!"
+#review_text = "I hate this. It is awful. Warning!"
+review_cleaned = clean_text(review_text)
+review_vector = vectorizer.transform([review_text])
+print(clf_svc.predict(review_vector))
--- mar29/step2.py
+++ mar29/step2.py
@@ -13,23 +13,23 @@ clothing = 'Clothing'
 
 categories = [books, books, clothing, clothing]
 
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(ngram_range=(2, 3))
 
 vectors = vectorizer.fit_transform(corpus)
 print(vectorizer.get_feature_names_out())
 print(vectors.toarray())
 
-svm = SVC(kernel='linear')
-svm.fit(vectors, categories)
+# svm = SVC(kernel='linear')
+# svm.fit(vectors, categories)
 
 
-test_X = [
-    'I love this read',
-    'such a nice hat',
-    'what a great book'
-]
+# test_X = [
+#     'I love this read',
+#     'such a nice hat',
+#     'what a great book'
+# ]
 
-test_y = [books, clothing, books]
-test_vectors = vectorizer.transform(test_X)
-print('The result is:', svm.score(test_vectors, test_y))
+# test_y = [books, clothing, books]
+# test_vectors = vectorizer.transform(test_X)
+# print('The result is:', svm.score(test_vectors, test_y))
 
